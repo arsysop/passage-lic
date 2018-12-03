4174c60eda0a6b5a486c48ea0db7e0613025fb94
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2018-12-03 11:55:28 +0300

    add generated NOT for getText

--- bundles/ru.arsysop.passage.lic.model.edit/src-gen/ru/arsysop/passage/lic/model/edit/providers/ProductVersionFeatureItemProvider.java
+++ bundles/ru.arsysop.passage.lic.model.edit/src-gen/ru/arsysop/passage/lic/model/edit/providers/ProductVersionFeatureItemProvider.java
@@ -180,7 +180,7 @@ public class ProductVersionFeatureItemProvider
 	 * This returns the label text for the adapted class.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	@Override
 	public String getText(Object object) {
